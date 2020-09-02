7029a2cd72b4461371232d4ed2d5e58b3394a991
caxtonMuthoni <githinjicaxton323@gmail.com>
2020-09-02 12:26:05 +0300

    Adding google search verification key

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { name: 'google-site-verification', content: 'YSkrq7cuBs2kFj7hPwp897439El7SDuib85NvSYYpwE' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
